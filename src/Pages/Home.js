020506fecfdf0a8702e8022aa8c81798e49f2892
andreyscott <andreyscott301@gmail.com>
2022-06-04 14:06:43 +0100

    maade it look good

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Cta } from '../component/Cta'
 import Developers from '../component/Developers'
-import Developer from '../component/test'
+// import Developer from '../component/test'
+import { Token } from '../component/Token'
 import { Wallet } from '../component/Wallet'
 
 export const Home = () => {
   return (
-    <div className=' bg-darkBg h-full text-white '>
+    <div className=' bg-darkBg h-full  overflow-hidden'>
 <Cta />
 <Wallet />
 <Developers />
-<Developer />
+<Token />
 </div>
     
   )
 }
+
 
